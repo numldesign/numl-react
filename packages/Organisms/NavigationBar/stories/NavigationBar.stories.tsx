99948e1bc9b03cd9e75baba9ad830cf53db0bc19
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-03-27 16:46:41 +0500

    fix: Navigation Bar with fixed settings and scrollable menu

--- packages/Organisms/NavigationBar/stories/NavigationBar.stories.tsx
+++ packages/Organisms/NavigationBar/stories/NavigationBar.stories.tsx
@@ -62,47 +62,5 @@ Default.args = {
       icon: 'subscription-outline',
       text: 'Subscription',
     },
-    ,
-    {
-      icon: 'download-outline',
-      text: 'Order',
-      count: '15',
-    },
-    {
-      icon: 'pricetag-outline',
-      text: 'Products',
-    },
-    {
-      icon: 'person-outline',
-      text: 'Customer',
-    },
-    {
-      icon: 'bar-chart-outline',
-      text: 'Analytics',
-    },
-    {
-      icon: 'megaphone-outline',
-      text: 'Marketing',
-    },
-    {
-      icon: 'discount-outline',
-      text: 'Discount',
-    },
-    {
-      icon: 'apps-outline',
-      text: 'Apps',
-    },
-    {
-      icon: 'page-builder-outline',
-      text: 'Page Builder',
-    },
-    {
-      icon: 'documents-outline',
-      text: 'Form Builder',
-    },
-    {
-      icon: 'subscription-outline',
-      text: 'Subscription',
-    },
   ],
 };
