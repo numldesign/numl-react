ebceed298bb349677aebdb319bd242c6351c3483
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-04-05 18:33:14 +0500

    fix: atoms build completed

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+// import { defineConfig } from 'vite'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   // plugins: [reactRefresh()]
+// })
+
+// vite.config.js
+const path = require('path');
+
+module.exports = {
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './packages/entry.ts'),
+      name: 'Avatar',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+};
