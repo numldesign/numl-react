f5ffa8db67f803b030f2e64c03bb32697f04cfd6
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-04-09 11:45:09 +0500

    fix : removig package json

--- packages/Atoms/Button/stories/Button.stories.tsx
+++ packages/Atoms/Button/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Root from '../../../Components/Root';
-import Button from '../src/Button';
+import { Button } from '../index';
 import { Grid } from './../../../Components/Grid/';
 import { Heading } from './../../../Components/Heading/src/Heading';
 
@@ -22,25 +22,25 @@ const Template = function (props: any) {
     <Root>
       <Grid columns="auto" rows="1fr" gap>
         <Grid.Row row="1">
-          <Grid.Column column="1" padding="0 1">
+          <Grid.Column column="1" padding="0x 2x">
             <Heading.Two>Default</Heading.Two>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row row="2">
-          <Grid.Column column="1" padding="0 1">
-            <Button {...otherProps} gap padding="0.4 0.6">
+          <Grid.Column column="1" padding="0x 2x">
+            <Button {...otherProps} gap padding="0.8x 1.2x">
               <Button.Label>{label}</Button.Label>
             </Button>
           </Grid.Column>
 
-          <Grid.Column column="2" padding="0 1">
-            <Button {...otherProps} gap padding="0.5 1">
+          <Grid.Column column="2" padding="0x 2x">
+            <Button {...otherProps} gap padding="1x 2x">
               <Button.Label>{label}</Button.Label>
             </Button>
           </Grid.Column>
 
-          <Grid.Column column="3" padding="0 1">
-            <Button {...otherProps} gap padding="0.6 1.2">
+          <Grid.Column column="3" padding="0x 2x">
+            <Button {...otherProps} gap padding="1.2x 1.2">
               <Button.Label>{label}</Button.Label>
               <Button.Icon name="chevron-down ^:pressed[chevron-up]"></Button.Icon>
             </Button>
@@ -48,25 +48,25 @@ const Template = function (props: any) {
         </Grid.Row>
 
         <Grid.Row row="3">
-          <Grid.Column column="1" padding="0 1">
+          <Grid.Column column="1" padding="0x 2x">
             <Heading.Two>Special</Heading.Two>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row row="4">
-          <Grid.Column column="1" padding="0 1">
-            <Button {...otherProps} theme="special" gap padding="0.4 0.6">
+          <Grid.Column column="1" padding="0x 2x">
+            <Button {...otherProps} theme="special" gap padding="0.8x 1.2x">
               <Button.Label>{label}</Button.Label>
             </Button>
           </Grid.Column>
 
-          <Grid.Column column="2" padding="0 1">
-            <Button {...otherProps} theme="special" gap padding="0.5 1">
+          <Grid.Column column="2" padding="0x 2x">
+            <Button {...otherProps} theme="special" gap padding="1x 2x">
               <Button.Label>{label}</Button.Label>
             </Button>
           </Grid.Column>
 
-          <Grid.Column column="3" padding="0 1">
-            <Button {...otherProps} theme="special" gap padding="0.6 1.2">
+          <Grid.Column column="3" padding="0x 2x">
+            <Button {...otherProps} theme="special" gap padding="1.2x 1.2">
               <Button.Label>{label}</Button.Label>
               <Button.Icon name="chevron-down ^:pressed[chevron-up]"></Button.Icon>
             </Button>
@@ -74,26 +74,26 @@ const Template = function (props: any) {
         </Grid.Row>
 
         <Grid.Row row="5">
-          <Grid.Column column="1" padding="0 1">
+          <Grid.Column column="1" padding="0x 2x">
             <Heading.Two>Danger</Heading.Two>
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row row="6">
-          <Grid.Column column="1" padding="0 1">
-            <Button {...otherProps} theme="danger" gap padding="0.4 0.6">
+          <Grid.Column column="1" padding="0x 2x">
+            <Button {...otherProps} theme="danger" gap padding="0.8x 1.2x">
               <Button.Label>{label}</Button.Label>
             </Button>
           </Grid.Column>
 
-          <Grid.Column column="2" padding="0 1">
-            <Button {...otherProps} theme="danger" gap padding="0.5 1">
+          <Grid.Column column="2" padding="0x 2x">
+            <Button {...otherProps} theme="danger" gap padding="1x 2x">
               <Button.Label>{label}</Button.Label>
             </Button>
           </Grid.Column>
 
-          <Grid.Column column="3" padding="0 1">
-            <Button {...otherProps} theme="danger" gap padding="0.6 1.2">
+          <Grid.Column column="3" padding="0x 2x">
+            <Button {...otherProps} theme="danger" gap padding="1.2x 1.2">
               <Button.Label>{label}</Button.Label>
               <Button.Icon name="chevron-down ^:pressed[chevron-up]"></Button.Icon>
             </Button>
@@ -101,26 +101,26 @@ const Template = function (props: any) {
         </Grid.Row>
 
         <Grid.Row row="7">
-          <Grid.Column column="1" padding="0 1">
+          <Grid.Column column="1" padding="0x 2x">
             <Heading.Two>Warning</Heading.Two>
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row row="8">
-          <Grid.Column column="1" padding="0 1">
-            <Button {...otherProps} theme="warning" gap padding="0.4 0.6">
+          <Grid.Column column="1" padding="0x 2x">
+            <Button {...otherProps} theme="warning" gap padding="0.8x 1.2x">
               <Button.Label>{label}</Button.Label>
             </Button>
           </Grid.Column>
 
-          <Grid.Column column="2" padding="0 1">
-            <Button {...otherProps} theme="warning" gap padding="0.5 1">
+          <Grid.Column column="2" padding="0x 2x">
+            <Button {...otherProps} theme="warning" gap padding="1x 2x">
               <Button.Label>{label}</Button.Label>
             </Button>
           </Grid.Column>
 
-          <Grid.Column column="3" padding="0 1">
-            <Button {...otherProps} theme="warning" gap padding="0.6 1.2">
+          <Grid.Column column="3" padding="0x 2x">
+            <Button {...otherProps} theme="warning" gap padding="1.2x 1.2">
               <Button.Label>{label}</Button.Label>
               <Button.Icon name="chevron-down ^:pressed[chevron-up]"></Button.Icon>
             </Button>
