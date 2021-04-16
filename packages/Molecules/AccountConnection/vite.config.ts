b469600216897827ad201a3e34b30caa000d2782
syedumerhasan <umerhasan2010@gmail.com>
2021-04-16 17:42:52 +0500

    fix: molecules

---
+++ packages/Molecules/AccountConnection/vite.config.ts
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  build: {
+    outDir: './packages/Molecules/AccountConnection/dist',
+    lib: {
+      entry: path.resolve(__dirname, './index'),
+      name: 'AccountConnection',
+      formats: ['es', 'cjs'],
+      minify: false,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+};
