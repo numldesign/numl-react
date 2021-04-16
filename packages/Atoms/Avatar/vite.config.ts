16d77f600f0f244ecb55e77b2b85c6bb29d1c862
syedumerhasan <umerhasan2010@gmail.com>
2021-04-16 18:24:52 +0500

    fix:  package.json issue fixed 2

--- packages/Atoms/Avatar/vite.config.ts
+++ packages/Atoms/Avatar/vite.config.ts
@@ -9,7 +9,7 @@ module.exports = {
       formats: ['es', 'cjs'],
       minify: false,
     },
-    root:path.resolve(__dirname, './index.ts'),
+    root: path.resolve(__dirname, './index.ts'),
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
