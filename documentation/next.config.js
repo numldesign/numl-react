e3e91a771d404ae84628269c42b1176fd0d95d04
syedumerhasan <umerhasan2010@gmail.com>
2021-04-20 22:49:11 +0500

    feat: layer0 documentation deployment added

--- documentation/next.config.js
+++ documentation/next.config.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { withLayer0 } = require('@layer0/next/config')
 const withMDX = require('@next/mdx')({
     extension: /\.mdx$/
 })
-  
-module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
-})
+
+module.exports = withLayer0(
+    withMDX({
+        pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+        typescript: {
+            // !! WARN !!
+            // Dangerously allow production builds to successfully complete even if
+            // your project has type errors.
+            // !! WARN !!
+            ignoreBuildErrors: true,
+        },
+    })
+)
+
