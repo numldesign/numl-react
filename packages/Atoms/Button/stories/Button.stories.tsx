d04b9be4ebad41a706f0e32b2b175929a84d1ee2
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-04-27 17:39:03 +0500

    fix: button fixed

--- packages/Atoms/Button/stories/Button.stories.tsx
+++ packages/Atoms/Button/stories/Button.stories.tsx
@@ -94,34 +94,34 @@ const Template3 = function ({ theme }) {
       <El.ThemeProvider name="secondary" hue="240" saturation="75"></El.ThemeProvider>
 
       <El.Base padding="2x">
-        <Button.Small size="0.5" theme={theme}>
+        <Button.Small theme={theme} height="" padding="0.5 2x" gap="2">
           <Button.Label size="sm">Button</Button.Label>
           <Button.DropDown size="sm"></Button.DropDown>
-          <Button.Popup size="sm"></Button.Popup>
+          <Button.Popup size="sm"> Lorem Ipsum</Button.Popup>
         </Button.Small>
       </El.Base>
 
       <El.Base padding="2x">
-        <Button.Medium theme={theme}>
+        <Button.Medium theme={theme} height="" padding="0.75 2x" gap="2">
           <Button.Label size="md">Button</Button.Label>
           <Button.DropDown size="md"></Button.DropDown>
-          <Button.Popup size="sm"></Button.Popup>
+          <Button.Popup size="md">Lorem Ipsum</Button.Popup>
         </Button.Medium>
       </El.Base>
 
       <El.Base padding="2x">
-        <Button.Large theme={theme}>
+        <Button.Large theme={theme} height="" padding="1x 3x" gap="2">
           <Button.Label size="lg">Button</Button.Label>
           <Button.DropDown size="lg"></Button.DropDown>
-          <Button.Popup size="sm"></Button.Popup>
+          <Button.Popup size="lg">Lorem Ipsum</Button.Popup>
         </Button.Large>
       </El.Base>
 
       <El.Base padding="2x">
-        <Button.ExtraLarge theme={theme}>
+        <Button.ExtraLarge theme={theme} height="" padding="1.5x 4x" gap="2">
           <Button.Label size="xl">Button</Button.Label>
           <Button.DropDown size="xl"></Button.DropDown>
-          <Button.Popup size="sm"></Button.Popup>
+          <Button.Popup size="xl">Lorem Ipsum</Button.Popup>
         </Button.ExtraLarge>
       </El.Base>
     </El.NumlProvider>
