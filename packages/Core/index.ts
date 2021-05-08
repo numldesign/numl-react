bf1e884001035c0eaa5e4b0b6f645e5b2dff935c
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-08 18:57:31 +0500

    fix: removing cyclic dependency

--- packages/Core/index.ts
+++ packages/Core/index.ts
@@ -1,17 +1,17 @@
 /** Export all declared Atoms */
-export * from '@numl-react/atoms';
+export * from './../Atoms';
 
 /** Application Configuration Numl Tags */
-export { NumlProvider, AttributeProvider, PropsProvider, ThemeProvider } from '@numl-react/elements';
+export { NumlProvider, AttributeProvider, PropsProvider, ThemeProvider } from './../Elements';
 
 /** Exporting all declared molecules */
-export * from '@numl-react/molecules';
+export * from './../Molecules';
 
 /**
  * Exporting all Atoms, Molucules and Elements,
  * So that user will have flexibility to access all
  * Atoms, Molecules and
  */
-export * as El from '@numl-react/elements';
-export * as Atoms from '@numl-react/atoms';
-export * as Molecules from '@numl-react/molecules';
+export * as El from './../Elements';
+export * as Atoms from './../Atoms';
+export * as Molecules from './../Molecules';
