3aba8f350331257f02f34b2b82fed3a3e08fe1bb
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-08 16:38:02 +0500

    fix: adding core

--- packages/Core/vite.config.ts
+++ packages/Core/vite.config.ts
@@ -11,7 +11,7 @@ module.exports = {
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react'],
+      external: ['react', '@numl-react/core', '@numl-react/atoms', '@numl-react/utility', '@numl-react/elements'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
