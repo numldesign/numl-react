1b068129444ef8b95b288b15f193390fef0fee1d
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-14 03:12:13 +0500

    fix: test storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,30 @@ module.exports = {
     '../packages/**/*.stories.mdx',
     '../packages/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  test: /\.(ts|tsx)$/,
+  typescript: {
+    check: true,
+    reactDocgen: 'react-docgen-typescript',
+  },
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport',
+    '@storybook/addon-docs',
+    '@storybook/preset-create-react-app',
+  ],
+  exclude: [
+    /node_modules/,
+    '/packages/**/*/*.test.ts',
+    '/packages/**/*/*.test.tsx',
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, '../packages/'),
+      'node_modules',
+    ];
+
+    return config;
+  },
 };
