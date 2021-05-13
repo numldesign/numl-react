9d40e09f2e5d9adb878f971c450f23e61c7d0606
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-14 00:58:16 +0500

    fix: test storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: MINIMAL_VIEWPORTS,
+  },
+};
