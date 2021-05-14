c16530625eb6dd527cfec11f20b6f366bb4fbd06
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-14 20:41:18 +0500

    fix: adding packahing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../packages/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
 };
