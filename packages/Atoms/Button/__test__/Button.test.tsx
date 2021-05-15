d3e4d633e49de22573cc5cc52de9f17534c7d081
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-15 19:12:18 +0500

    fix: button test

--- packages/Atoms/Button/__test__/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Button } from '../index';
-
-it('Renders without crashing with children and clear attribute', async () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Button> Hello World!</Button>, div);
-});
