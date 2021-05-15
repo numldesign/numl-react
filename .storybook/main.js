1de5608cf531d850550adb206047a3626a623a30
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-15 13:41:28 +0500

    fix: version issue fixed

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,8 @@ module.exports = {
   stories: ['../packages/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    '@storybook/preset-create-react-app',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
   ],
