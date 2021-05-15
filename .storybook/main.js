d31b6b2e8906edbae4d14823d8ec659f1560804b
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-15 18:08:00 +0500

    fix: storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,4 @@
 module.exports = {
   stories: ['../packages/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-knobs',
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
