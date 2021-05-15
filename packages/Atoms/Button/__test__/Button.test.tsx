61d4c5e520f0cf4bc9506972a1ec55683bc52302
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-15 18:42:47 +0500

    fix: button test

--- packages/Atoms/Button/__test__/Button.test.tsx
+++ packages/Atoms/Button/__test__/Button.test.tsx
@@ -6,13 +6,3 @@ it('Renders without crashing with children and clear attribute', async () => {
   const div = document.createElement('div');
   ReactDOM.render(<Button> Hello World!</Button>, div);
 });
-
-it('Renders without crashing with children and special attribute', async () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Button special> Hello World!</Button>, div);
-});
-
-it('Renders without crashing with children and toggle attribute', async () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Button toggle> Hello World!</Button>, div);
-});
