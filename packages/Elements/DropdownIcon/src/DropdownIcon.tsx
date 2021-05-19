539d9c97886a6304b7dd9bcd2b4f64019165edb5
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-19 21:34:49 +0500

    fix(elements): Dropdown Icon Size

--- packages/Elements/DropdownIcon/src/DropdownIcon.tsx
+++ packages/Elements/DropdownIcon/src/DropdownIcon.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const DropdownIcon = React.forwardRef((props: any, ref) => {
-  const { children, ...otherProps } = props;
+  const { children, theme, ...otherProps } = props;
 
-  otherProps['size'] = '1.25rem';
+  otherProps.size = '1.25rem';
+  otherProps.padding = '0 1x';
+  otherProps.theme = theme || '0 1x';
 
   return React.createElement(
     'nu-dropdownicon',
