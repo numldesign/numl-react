29334772afe8cc3e45e3e4577532a97713085c54
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-05-23 17:00:37 +0500

    fix: ifta labels

--- packages/Elements/Icon/src/Icon.ts
+++ packages/Elements/Icon/src/Icon.ts
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const Icon = React.forwardRef((props: any, ref) => {
-  const { children, padding, ...otherProps } = props;
+  const { children, ...otherProps } = props;
 
   otherProps.size = '1.25rem';
-  otherProps.padding = padding || '0 0.25x';
 
   return React.createElement(
     'nu-icon',
