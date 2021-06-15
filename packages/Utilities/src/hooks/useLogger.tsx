bf14027913a144d7cca4c88adb5d06d6acf0314c
Syed Umer Hasan <umerhasan2010@gmail.com>
2021-06-15 19:17:21 +0500

    fix: adding custom hooks

--- packages/Utilities/src/hooks/useLogger.tsx
+++ packages/Utilities/src/hooks/useLogger.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const useLogger = (value) => {
+const useLogger = (value: any) => {
   React.useEffect(() => {
     // eslint-disable-next-line no-console
     console.log(value);
